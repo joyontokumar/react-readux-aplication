3aa7fb41a5339840cd44e30b602a41a488963a33
joyonto kumar <engr.joyontoroy@gmail.com>
2020-02-22 00:15:51 +0600

    base url change

--- src/common.js
+++ src/common.js
@@ -3,5 +3,5 @@ export const base_url = "http://localhost:3000/";
 export const image_path = "http://localhost:3000/img/";
 
 // live server
-// export const base_url = "https://btlbd.xyz/waiter/";
-// export const image_path = "https://btlbd.xyz/waiter/img/";
+// export const base_url = "https://hamaronline.com/redux_starter/";
+// export const image_path = "https://hamaronline.com/redux_starter/img/";
